bc2e4040adba2bad7f59d8ccf1094dedac1d3c2e
Joseph Immanuel Navarro <navarro0@purdue.edu>
2017-12-04 11:30:08 -0500

    Fixed kanji positioning

--- constant.py
+++ constant.py
@@ -9,8 +9,8 @@ SIZE  = W,H = 800,600      ## Screen size
 FPS   = 60                 ## Screen refresh rate
 
 ## GUI button placement
-KANJI_VERT = [H//2 for n in range(2)]
-KANJI_HORZ = [W*3//8, W*5//8]
+KANJI_VERT = [H//3 for n in range(2)]
+KANJI_HORZ = [W*3//9, W*6//9]
 BTNVERT = [480     for n in range(3)] ## Button y-coordinates
 BTNHORZ = [W/(3-n) for n in range(3)] ## Button x-coordinates
 
