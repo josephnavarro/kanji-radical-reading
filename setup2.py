c925c7b59501c02f1aaf7c808e6ee37543fd342f
Joseph Immanuel Navarro <navarro0@purdue.edu>
2017-12-06 03:23:49 -0500

    Finally implemented setup and exe freezing

--- setup2.py
+++ setup2.py
@@ -1,5 +1,22 @@
 from distutils.core import setup
-import py2exe
+import py2exe, os, glob
+
+dfiles = []
+
+rootdir = 'C:/Users/Joey Schamoley/Desktop/games/'
+
+def get_files(dfiles, directory):
+    d1 = rootdir + directory
+    for files in os.listdir(d1):
+        f1 = d1 + files
+        if os.path.isfile(f1):
+            f2 = directory, [f1]
+            dfiles.append(f2)
+        else:
+            get_files(dfiles, directory + files + '/')
+
+get_files(dfiles, 'res/')
+
 setup(
     version = "0.0001",
     description = "Final project for JPNS401 at Purdue University",
@@ -16,8 +33,11 @@ setup(
         'copyright': "Copyright (c) 2017 Joey Navarro"
     }],
     options = {'py2exe': {
-        'optimize': 2, 'bundle_files': 1, 'compressed': True, \
-        'excludes': [], 'packages': [], \
+        'optimize': 2,
+        'bundle_files': 1,
+        'compressed': True,
+        'excludes': [],
+        'packages': [],
         'dll_excludes': [''],
         'includes': [
         'button',
@@ -26,8 +46,18 @@ setup(
         'stage',
         'text',
         'utility',],
+        'excludes':[
+            'setup2',
+            ],
         'dist_dir': 'outputs'}
                },
     zipfile = None,
-    data_files = ['res'],
+    data_files = [
+        ('res/base', glob.glob('res/base/*.png')),
+        ('res/data', glob.glob('res/data/*.config')),
+        ('res/font', glob.glob('res/font/*.ttf')),
+        ('res/img', glob.glob('res/img/*.png') + glob.glob('res/img/*.ico')),
+        ('res/kanji', glob.glob('res/kanji/*.png')),
+        ('res/radical', glob.glob('res/radical/*.png')),
+        ('res/snd', glob.glob('res/snd/*.ogg')),],
     )
