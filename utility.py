698178af9a0ef6434dfe9d2f69d63b75547f5e5d
Joey Navarro <therealjoeynavarro@aol.com>
2023-12-30 03:26:41 -0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,34 +1,59 @@
-#!usr/bin/env python
-import pygame, glob, os
-from   pygame.locals    import *
-from   pygame.transform import scale  as sc
-from   pygame.transform import rotate as rot
-from   constant         import *
+"""
+Universal functions
+"""
+import pygame
+import glob
+from pygame.locals import *
+from pygame.transform import scale as sc
+from pygame.transform import rotate as rot
+from constant import *
 
-## Functions used universally
 
 def _quit():
-    ## Safely quit game
+    """
+    Safely quits pygame.
+    """
     pygame.quit()
     raise SystemExit
 
+
 def apply_vector(vector, *fxns):
-    ## Applies a function over all elements
+    """
+    Mutates an input iterable by applying one or more functions over each
+    of its elements.
+
+    Returns a copy of the input iterable, as modified in this way.
+    """
     for fxn in fxns:
         vector = [fxn(v) for v in vector[:]]
 
     return vector
 
+
 def smooth_rotate(img, angle, scale=MINISCALE):
-    ## Rotates and scales
-    size = apply_vector(img.get_size(), lambda x:x*scale, round, int)
-    img  = sc(img, size)
-    img  = rot(img, angle)
+    """
+    Rotates and scales an image. Returns the image.
+    """
+    # Calculate new integer dimensions
+    size = apply_vector(
+        img.get_size(),
+        lambda x: x * scale,
+        round,
+        int,
+    )
+    
+    # Scale
+    img = sc(img, size)
+    
+    # Rotate
+    img = rot(img, angle)
+    
     return img
 
-def get_words(path, b, ext=PNGWILD):
+
+def get_words(path, b, ext=PNGWILD) -> dict:
     ## Get kanji from images and pair them with strings
-    d = {}
+    d: dict = {}
     for fn in glob.glob(path + ext):
         if b in fn:
             im = load_image(fn)
@@ -37,10 +62,11 @@ def get_words(path, b, ext=PNGWILD):
 
     return d
 
-def get_bases(path, b, ext=PNGWILD):
+
+def get_bases(path, b, ext=PNGWILD) -> dict:
     ## Get kanji from images and pair them with strings
-    full  = None
-    none  = None
+    full = None
+    none = None
     for fn in glob.glob(path + ext):
         if b in fn:
             if KEY_NONE in fn:
@@ -48,16 +74,23 @@ def get_bases(path, b, ext=PNGWILD):
             else:
                 full = load_image(fn)
 
-    return {KEY_NONE:none, KEY_FULL:full}
+    return {
+        KEY_NONE: none,
+        KEY_FULL: full
+    }
+
 
 def get_scaling():
-    ## Gets ratio between window size and interal screen
+    """
+    Calculates ratio between true window size and internal screen dimensions
+    """
     w1, h1 = pygame.display.get_surface().get_size()
-    return W/w1, H/h1
+    return W / w1, H / h1
+
 
 def get_input(events):
     ## Get mouse input
-    x,y = -1,-1
+    x, y = -1, -1
     for e in events:
         if e.type == QUIT:
             ## Click [x] to exit window
@@ -72,68 +105,92 @@ def get_input(events):
         
         elif e.type == MOUSEBUTTONDOWN:
             ## Get mouse pos on click
-            x,y = pygame.mouse.get_pos()
+            x, y = pygame.mouse.get_pos()
         
         elif e.type == MOUSEBUTTONUP:
             ## Get mouse pos on click
-            x,y = pygame.mouse.get_pos()
+            x, y = pygame.mouse.get_pos()
 
-    sx,sy = get_scaling()
+    sx, sy = get_scaling()
 
-    return x*sx, y*sy
+    return x * sx, y * sy
 
 
 def null_function():
-    ## Do-nothing function returned by default from a button
+    """
+    Do-nothing function returned by default from a button
+    """
     pass
 
-def load_image(fn):
-    ## Load image with alpha transparency
-    return pygame.image.load(fn).convert_alpha()
+
+def load_image(filename: str) -> pygame.Surface:
+    """
+    Loads an image with alpha transparency.
+    """
+    return pygame.image.load(filename).convert_alpha()
+
     
-def clean_line(s, j=' '):
-    ## Removes in-line comments from script
+def clean_line(s: str, j: str = ' ') -> str:
+    """
+    Normalizes whitespace within the contents of a string,
+    """
     return j.join(s.split())
 
+
 def extract_lines(f):
-    ## Removes comments from script
+    """
+    Removes comments from the contents of a script file
+    """
     l = f.readlines()
     o = apply_vector(l, clean_line)
-    return [p for p in o if len(p) != 0]
-
-def split(s, d):
-    ## Splits a string according to a delimiter
-    return s.split(d)
+    return [p for p in o if bool(p)]
 
 
-def add_entry(d, k, v):
-    ## Adds a value to a dictionary
-    if k in d.keys():
-        d[k] = list(d[k])
-        d[k].append(v)      
+def split(string: str, delim: str) -> list:
+    """
+    Splits a string according to a delimiter
+    """
+    return string.split(delim)
+
+
+def add_entry(d: dict, key, val):
+    """
+    Adds a value to a dictionary, ensuring it's stored in a list
+    """
+    if key not in d.keys():
+        d[key] = [val]
     else:
-        d[k] = [v]
+        d[key] = list(d[key])
+        d[key].append(val)
 
 
-def make_dict(p, fxn):
-    ## Instantiates a new dictionary
-    o = {}
+def make_dict(p, fxn) -> dict:
+    """
+    Instantiates a new dictionary, ensuring each of its values are lists
+    """
+    o: dict = {}
     for q in p:
         add_entry(o, q[0], fxn(q[1]))
     return o
 
-def convert_int(s):
-    ## Converts a string to an int
+
+def convert_int(s: str) -> int:
+    """
+    Converts a string to an integer.
+    """
     return int(s)
     
     
-def parse(fn, fxn=lambda x:x):
+def parse(filename, fxn=lambda x: x) -> dict:
+    """
+    Parses a script file and makes a dictionary mapping out of it.
+    
+    Default function to apply is the identity function.
+    """
     ## Parses a file and makes a dictionary
-    d = {}
-    with open(fn, "r", encoding='utf-8') as f:
+    d: dict = {}
+    with open(filename, "r", encoding='utf-8') as f:
         l = extract_lines(f)
-        d = make_dict([split(m,COLON) for m in l], fxn)
+        d = make_dict([split(m, COLON) for m in l], fxn)
         
     return d
-
-
